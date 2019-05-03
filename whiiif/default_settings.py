94cbef593b76826eb801e8268f3e6aa7c4729978
Mike Bennett <mike.bennett@ed.ac.uk>
2019-05-03 13:54:42 +0100

    default settings for XML file location

--- whiiif/default_settings.py
+++ whiiif/default_settings.py
@@ -1,4 +1,7 @@
 DEBUG = True  # make sure DEBUG is off unless enabled explicitly otherwise
+FLASK_ENV = 'development'
+SERVER_NAME = 'localhost:5000'
 LOG_DIR = '.'  # create log files in current working directory
 SOLR_URL = 'http://localhost:8983/solr' # assume local solr instance
 SOLR_CORE = 'whiiif' # default core name
+XML_LOCATION  = '../xml' # location to store ascii-escaped XML files. must be same as PathFieldLoader in solr config
