b7d0bc36e5253d786043950bf1190ff362e88f4a
Mike Bennett <mike.bennett@ed.ac.uk>
2020-04-30 18:44:46 +0100

    Start to use semver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='whiiif',
-    version='1.0',
+    version='0.2',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
