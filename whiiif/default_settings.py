581840afe08dcd4e315c360f5f49c10e8648a363
Mike Bennett <mike.bennett@ed.ac.uk>
2020-05-05 19:44:37 +0100

    reformat settings layout into required and optional settings (#2)

--- whiiif/default_settings.py
+++ whiiif/default_settings.py
@@ -1,3 +1,4 @@
+# REQUIRED SETTINGS
 # General Whiiif Settings
 SERVER_NAME = 'localhost:5000'
 LOG_DIR = '.'  # create log files in current working directory
@@ -5,11 +6,15 @@ LOG_DIR = '.'  # create log files in current working directory
 # SOLR Configuration
 SOLR_URL = 'http://localhost:8983/solr'  # assume local solr instance
 SOLR_CORE = 'whiiif'  # default core name
-
-# Common Search options
 OCR_TEXT_FIELD = 'ocr_text'  # the name of the field in the SOLR schema that contains the Whiiif-ingested XML data
 MANIFEST_URL_FIELD = 'manifest_url'  # name of the field containing the URL of the manifest
 DOCUMENT_ID_FIELD = 'id'  # the id field used for search urls
+
+# External files
+XML_LOCATION = '/opt/whiiif/resources/xml'  # location to store ALTO-XML files. must be same as PathFieldLoader in solr config
+MANIFEST_LOCATION = '/opt/whiiif/resources/manifests'  # location of the IIIF manifests
+
+# OPTIONAL SETTINGS (i.e if they are missing the application won't die!)
 # Search within
 WITHIN_MAX_RESULTS = 4096  # Max results when searching inside using IIIF Search, so should be quite high
 # Snippet search
@@ -24,6 +29,4 @@ COLLECTION_SNIPPET_CONTEXT = 'word'  # context for the returned snippets - can b
 COLLECTION_SNIPPET_CONTEXT_SIZE = 5  # number of context objects to return either side of the result
 COLLECTION_SNIPPET_CONTEXT_LIMIT = 'block'  # don't extend the context beyond this container object
 
-# External files
-XML_LOCATION = '/opt/whiiif/resources/xml'  # location to store ALTO-XML files. must be same as PathFieldLoader in solr config
-MANIFEST_LOCATION = '/opt/whiiif/resources/manifests'  # location of the IIIF manifests
+
