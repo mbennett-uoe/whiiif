46534742509e785eddb4c509d0c1e1014b1bdd5b
Mike Bennett <mike.bennett@ed.ac.uk>
2020-05-05 19:33:47 +0100

    Add tests for resource directory presence (#6)

--- tests/test_whiiif.py
+++ tests/test_whiiif.py
@@ -1,16 +1,25 @@
 import unittest
 
 import whiiif
+import os.path
 
-
-class WhiiifTestCase(unittest.TestCase):
+class BaseAppTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = whiiif.app.test_client()
+        self.config = whiiif.app.config
 
     def test_index(self):
         rv = self.app.get('/')
         self.assertIn('Welcome to Whiiif', rv.data.decode())
+
+    def test_manifest_dir(self):
+        manifest_path = self.config["MANIFEST_LOCATION"]
+        self.assertTrue(os.path.isdir(manifest_path))
+
+    def test_xml_dir(self):
+        xml_path = self.config["XML_LOCATION"]
+        self.assertTrue(os.path.isdir(xml_path))
 
 
 if __name__ == '__main__':
