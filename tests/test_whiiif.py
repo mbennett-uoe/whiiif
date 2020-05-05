f5f3525fb32ab6468469d066f58e93651a8bdfc8
Mike Bennett <mike.bennett@ed.ac.uk>
2020-05-05 19:49:08 +0100

    reorder tests and add comments (#6)

--- tests/test_whiiif.py
+++ tests/test_whiiif.py
@@ -11,18 +11,12 @@ class BaseAppTestCase(unittest.TestCase):
         self.config = whiiif.app.config
 
     def test_index(self):
+        # Can we get the homepage?
         rv = self.app.get('/')
         self.assertIn('Welcome to Whiiif', rv.data.decode())
 
-    def test_manifest_dir(self):
-        manifest_path = self.config["MANIFEST_LOCATION"]
-        self.assertTrue(os.path.isdir(manifest_path))
-
-    def test_xml_dir(self):
-        xml_path = self.config["XML_LOCATION"]
-        self.assertTrue(os.path.isdir(xml_path))
-
     def test_config(self):
+        # Do all the required config variables exist?
         self.assertIn("SERVER_NAME", self.config)
         self.assertIn("LOG_DIR", self.config)
         self.assertIn("SOLR_URL", self.config)
@@ -30,6 +24,19 @@ class BaseAppTestCase(unittest.TestCase):
         self.assertIn("OCR_TEXT_FIELD", self.config)
         self.assertIn("MANIFEST_URL_FIELD", self.config)
         self.assertIn("DOCUMENT_ID_FIELD", self.config)
+        self.assertIn("MANIFEST_LOCATION", self.config)
+        self.assertIn("XML_LOCATION", self.config)
+
+    def test_manifest_dir(self):
+        # Does the manifest directory exist?
+        manifest_path = self.config["MANIFEST_LOCATION"]
+        self.assertTrue(os.path.isdir(manifest_path))
+
+    def test_xml_dir(self):
+        # Does the xml directory exist?
+        xml_path = self.config["XML_LOCATION"]
+        self.assertTrue(os.path.isdir(xml_path))
+
 
 if __name__ == '__main__':
     unittest.main()
