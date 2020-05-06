d8ce8cbfe8dae855431d0a623f71c6984a875f88
Mike Bennett <mike.bennett@ed.ac.uk>
2020-05-06 14:37:24 +0100

    Remove stuff that should be part of integration or deployment tests and add first unit test for search endpoint using mock request

--- tests/test_whiiif.py
+++ tests/test_whiiif.py
@@ -1,42 +1,67 @@
 import unittest
-
-import whiiif
+from unittest import mock
+from unittest.mock import patch
+from whiiif import app
 import os.path
+
+
+class FakeResponse(object):
+    status_code = 200
+
+    def json(self):
+        return {}
+
+
+def fake_request(url):
+    return FakeResponse()
 
 
 class BaseAppTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.app = whiiif.app.test_client()
-        self.config = whiiif.app.config
+        app.config['TESTING'] = True
+        app.config['SERVER_NAME'] = 'localhost:5000'
+        app.config['SOLR_URL'] = 'http://test/solr'
+        app.config['SOLR_CORE'] = 'whiiiftest'
+        app.config['OCR_TEXT_FIELD'] = 'ocr_text'
+        app.config['MANIFEST_URL_FIELD'] = 'manifest_url'
+        app.config['DOCUMENT_ID_FIELD'] = 'id'
+        self.app = app.test_client()
 
     def test_index(self):
         # Can we get the homepage?
         rv = self.app.get('/')
         self.assertIn('Welcome to Whiiif', rv.data.decode())
 
-    def test_config(self):
-        # Do all the required config variables exist?
-        self.assertIn("SERVER_NAME", self.config)
-        self.assertIn("LOG_DIR", self.config)
-        self.assertIn("SOLR_URL", self.config)
-        self.assertIn("SOLR_CORE", self.config)
-        self.assertIn("OCR_TEXT_FIELD", self.config)
-        self.assertIn("MANIFEST_URL_FIELD", self.config)
-        self.assertIn("DOCUMENT_ID_FIELD", self.config)
-        self.assertIn("MANIFEST_LOCATION", self.config)
-        self.assertIn("XML_LOCATION", self.config)
-
-    def test_manifest_dir(self):
-        # Does the manifest directory exist?
-        manifest_path = self.config["MANIFEST_LOCATION"]
-        self.assertTrue(os.path.isdir(manifest_path))
-
-    def test_xml_dir(self):
-        # Does the xml directory exist?
-        xml_path = self.config["XML_LOCATION"]
-        self.assertTrue(os.path.isdir(xml_path))
+    def test_search(self):
+        with patch("requests.get") as mock_request:
+            mock_request.return_value = FakeResponse()
+            rv = self.app.get('/search/test-manifest?q=myquery')
+            mock_request.assert_called_once_with("http://test/solr/whiiiftest/select?hl=on&"
+                                                 "hl.ocr.absoluteHighlights=true&hl.weightMatches=true"
+                                                 "&hl.ocr.limitBlock=page&hl.ocr.contextSize=1"
+                                                 "&hl.ocr.contextBlock=word&df=ocr_text&hl.ocr.fl=ocr_text"
+                                                 "&hl.snippets=4096&fq=id:test-manifest&q=myquery")
+        print(rv.data)
 
 
 if __name__ == '__main__':
     unittest.main()
+
+#               'DEBUG': True,
+#
+
+
+#               'XML_LOCATION': '/test/xml',
+#               'MANIFEST_LOCATION': '/test/manifests',
+#               'WITHIN_MAX_RESULTS': 128,
+#               'SNIPPETS_MAX_RESULTS': 3,
+#               'SNIPPET_CONTEXT': 'word',
+#               'SNIPPET_CONTEXT_SIZE': 5,
+#               'SNIPPET_CONTEXT_LIMIT': 'line',
+#               'COLLECTION_MAX_DOCUMENT_RESULTS': 2,
+#               'COLLECTION_MAX_RESULTS': 20,
+#               'COLLECTION_SNIPPET_CONTEXT': 'word',
+#               'COLLECTION_SNIPPET_CONTEXT_SIZE': 5,
+#               'COLLECTION_SNIPPET_CONTEXT_LIMIT': 'page',
+#                }
