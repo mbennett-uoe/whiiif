aea9b02bfd4cbb1979c2c9d8b500a5396bb1fd04
Mike Bennett <mike.bennett@ed.ac.uk>
2020-05-08 18:31:28 +0100

    improve exception handling for snippets (#4)

--- whiiif/views.py
+++ whiiif/views.py
@@ -251,7 +251,8 @@ def snippet_search(id):
         app.logger.debug("Solr JSON response code: {}".format(results_json["responseHeader"]["status"]))
         docs = results_json["response"]["docs"]
     except (requests.exceptions.ConnectionError, KeyError, ValueError) as e:
-        app.log_exception(e)
+        app.logger.error("Error occurred with SOLR query: {}".format(type(e)))
+        app.logger.error("Error message: {}".format(e))
         results_json = {}
         docs = []
 
