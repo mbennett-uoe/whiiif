163f5c837f05cb5f2b8b84d15ab5593d952d41e5
Mike Bennett <mike.bennett@ed.ac.uk>
2020-05-11 15:48:43 +0100

    fill out some metadata for the setup compilation script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='whiiif',
     version='2.0.0',
-    long_description=__doc__,
+    long_description=" ",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
@@ -13,4 +13,16 @@ setup(
         'lxml',
         'bleach'
     ],
+    author="Mike Bennett",
+    author_email="mike.bennett@ed.ac.uk",
+    description="Whiiif - Word Highlighting (in) IIIF. Whiiif is an implementation of the IIIF Search API designed to provide full-text search with granular, word-level Annotation results to enable front-end highlighting.",
+    url="https://mbennett-uoe.github.io/whiiif/", 
+    project_urls={
+        "Bug Tracker": "https://github.com/mbennett-uoe/whiiif/issues",
+        "Documentation": "https://mbennett-uoe.github.io/whiiif/",
+        "Source Code": "https://github.com/mbennett-uoe/whiiif",
+    },
+    license="LGPL v3",
+    license_file="LICENSE.md",
+    platforms=["linux","windows"]
 )
